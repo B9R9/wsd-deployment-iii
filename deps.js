068d93f58b03521c2be4e3ec5d8399782ee26745
B9R9 <baptiste.riffard@gmail.com>
2024-09-17 10:51:11 +0300

    Fix postgres function

--- deps.js
+++ deps.js
@@ -1,4 +1,4 @@
 export { serve } from "https://deno.land/std@0.222.1/http/server.ts";
 export { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
-export default { postgres };
+import { Client } from "https://deno.land/x/postgres/mod.ts";
+export { Client as postgres };
